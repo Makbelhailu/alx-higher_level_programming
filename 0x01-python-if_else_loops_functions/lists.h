a0b00c444ce61e67222f3ec285e5fba2770ba7e4
Makbelhailu <mcwelson92@gmail.com>
2022-08-23 10:21:45 -0700

    python is cool

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /*
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,7 +13,7 @@
 typedef struct listint_s
 {
 	int n;
-	struct istint_s *next;
+	struct listint_s *next;
 } listint_t;
 
 listint_t *add_nodeint_end(listint_t **head, const int n);
