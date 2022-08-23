96b01f115783c7f07917fe66de91a42c33dd0f01
Makbelhailu <mcwelson92@gmail.com>
2022-08-23 09:03:58 -0700

    python is cool

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        digit = number % -10
+        digit = -number % 10
     elif number > 0:
         digit = number % 10
     print("{:d}".format(digit), end="")
