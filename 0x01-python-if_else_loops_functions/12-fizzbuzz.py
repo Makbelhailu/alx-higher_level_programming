aae1126648f53e481b9b96306ba1409a5fe5d34d
Makbelhailu <mcwelson92@gmail.com>
2022-08-23 09:55:26 -0700

    python is cool

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,9 +6,9 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 != 0 and i % 5 != 0:
             print("{:d} ".format(i), end="")
-        elif i % 3 == 0:
+        elif i % 3 == 0 and i % 5 != 0:
             print("{:s} ".format(f), end="")
-        elif i % 5 == 0:
+        elif i % 5 == 0 and i % 3 != 0:
             print("{:s} ".format(b), end="")
         elif i % 5 == 0 and i % 3 == 0:
             print("{:s} ".format(fb), end="")
