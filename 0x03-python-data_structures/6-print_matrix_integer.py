87b480978f2aaccea860bb79b590914847099fe5
Makbelhailu <mcwelson92@gmail.com>
2022-08-27 02:56:42 -0700

    python is cool

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print("{}".format(""))
+        print("{}".format("")
     else:
         for i in range(len(matrix)):
-            for j in range(len(matrix[i][:])):
-                if j != len(matrix[i][:]):
-                    print("{} ".format(matrix[i][j]), end="")
-                if j == len(matrix[i][:]):
-                    print("{}".format(matrix[i][j]), end="")
-            print("{}".format(""))
+            for j in range(len(matrix[i])):
+                if j != 0:
+                    print(" ", end="")
+                print("{}".format(matrix[i][j]), end="")
+            print()
