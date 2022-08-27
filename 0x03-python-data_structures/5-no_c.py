77a9acc604f2471ec53ce6bfd786ec4ef3efbd40
Makbelhailu <mcwelson92@gmail.com>
2022-08-27 01:55:11 -0700

    python is cool

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     j = 0
     for i in range(a):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            new = my_string[:(i - j)] + my_string[i + 1:]
+            new = my_string[:(i - j)] + my_string[(i + 1):]
             j += 1
     return new
