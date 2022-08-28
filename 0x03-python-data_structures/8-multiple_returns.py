e7b0737799a3637f25dde972f224ec5eaf391b3c
Makbelhailu <mcwelson92@gmail.com>
2022-08-28 01:20:43 -0700

    python is cool

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     leng = len(sentence)
-    first = sentence[0]
+    if lang != 0:
+        first = sentence[0]
+    else:
+        first = None
     new = leng, first
     return (new)
