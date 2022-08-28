c461ccd26c625d143b84effacd38682c26be3451
Makbelhailu <mcwelson92@gmail.com>
2022-08-28 02:32:52 -0700

    python is cool

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,5 @@ def delete_at(my_list=[], idx=0):
     leng = len(my_list)
     if idx < 0 or idx > leng:
         return (my_list)
-    new = my_list
-    del new[idx]
-    return (new)
+    del my_list[idx]
+    return (my_list))
