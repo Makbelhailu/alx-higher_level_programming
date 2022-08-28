54201a34e392d587571fd59e5c377834f2f939b2
Makbelhailu <mcwelson92@gmail.com>
2022-08-28 01:37:38 -0700

    python is cool

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    a = len(my_list)
+    for i in range(a):
+        if my_list[i] % 2:
+            new[i] = True
+        else:
+            new[i] = False
+    return (new)
