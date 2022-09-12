07ca8f28438c5167bbb19d0c6fda328e84d5534b
Makbelhailu <mcwelson92@gmail.com>
2022-09-12 10:48:04 -0700

    python is cool

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,9 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(args[0], args[1])
-    except (IndexError, ValueError, TypeError, ZeroDivisionError) as er:
-        print(f"Exception: {er}", file=sys.stderr)
-        return None
-    except (SyntaxError, RuntimeError):
+    except (IndexError, ZeroDivisionError) as er:
         print(f"Exception: {er}", file=sys.stderr)
         return None
