55fbf376cc65e6aeaa5ea7ead9c2b1d01ddd7032
Makbelhailu <mcwelson92@gmail.com>
2022-09-13 05:12:36 -0700

    python is cool

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""class with square size and area"""
+
+
+class Square:
+    """Create a class with size and its area"""
+
+    def __init__(self, size=0):
+        """Constractor of square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculate its area"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """property for retriving its size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """setter for Constractor of square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
