9cceeb46c970a9590d6f942fe6ea95d9c4500b41
Makbelhailu <mcwelson92@gmail.com>
2022-09-13 06:37:07 -0700

    python is cool

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Class with size and position
+- area method
+- my_print method to print the area
+- setter and getter
+"""
+
+
+class Square:
+    """create size of square and position"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """constractor of square and its position"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        if len(position) != 2 or position[0] < 0 or position[1] < 0 \
+                or type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        """return the area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @propety
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if len(value) != 2 or type(value) is not tuple \
+                or value[0] is not int \
+                or value[1] is not int \
+                or (value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
