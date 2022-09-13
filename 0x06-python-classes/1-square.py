0525e2e604c789cf21f3cdec7dd6fa2e6c14268b
Makbelhailu <mcwelson92@gmail.com>
2022-09-13 04:01:19 -0700

    python is cool

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square with size"""
+
+
+class Square:
+    """Class to create a square with size"""
+
+    def __int__(self, square_size):
+        """Constracting a private size"""
+        self.__size = square_size
