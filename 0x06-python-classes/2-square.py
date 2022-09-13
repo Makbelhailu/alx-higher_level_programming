78dd142abf063674557f3f6a4fb181d8fb977e93
Makbelhailu <mcwelson92@gmail.com>
2022-09-13 04:35:28 -0700

    python is cool

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,9 @@ class Square:
     """Class to create size of square"""
 
     def __init__(self, size=0):
-        if size > 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        elif not size.isdigit():
+        elif (type(size) is not int)::
             raise TypeError("size must be an integer")
         else:
             self.__size = size
