e46b5aa4a1a2f324e7d67b224b42cc44e1a9d7cd
Makbelhailu <mcwelson92@gmail.com>
2022-09-13 05:38:18 -0700

    python is cool

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -17,13 +17,11 @@ class Square:
 
     def my_print(self):
         """print the area with # form"""
-        if self.__size != 0:
+        if self.__size > 0:
             for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#",end="")
-                print("")
+                print("#" * sel.__size)
         else:
-            print("")
+            print()
 
     @property
     def size(self):
