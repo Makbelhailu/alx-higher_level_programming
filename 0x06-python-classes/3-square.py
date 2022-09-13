03ef0a9b0d2bf3b49fe334cabd03f3b34eeb154a
Makbelhailu <mcwelson92@gmail.com>
2022-09-13 04:53:34 -0700

    python is cool

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,11 +6,14 @@ class Square:
     """Create size of a class and its area"""
 
     def __init__(self, size=0):
+        """Constractor of size"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
-        return self.__size ** 2
+        """calculate its area"""
+        return (self.__size ** 2)
