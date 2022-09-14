5ff45ff412e1ba440c3fc35b7d9fc81082326b14
Makbelhailu <mcwelson92@gmail.com>
2022-09-14 09:46:55 -0700

    python is cool

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,12 +1,12 @@
 #!usr/bin/python3
-"""class with square"""
+"""Class with square"""
 
 
 class Square:
-    """square - size - area"""
+    """Square - size - area"""
 
     def __init__(self, size=0):
-        """class size"""
+        """Class size"""
         if type(size) is not int:
             raise TypeError("size must be a number")
         elif size < 0:
@@ -15,41 +15,41 @@ class Square:
             self.__size = size
 
     def __lt__(self, other):
-        """compare with <"""
+        """Compare with <"""
         return (self.area() < other.area())
 
     def __gt__(self, other):
-        """ compare with >"""
+        """Compare with >"""
         return (self.area() > other.area())
 
     def __le__(self, other):
-        """compare with <="""
+        """Compare with <="""
         return (self.area() <= other.area())
 
     def __ge__(self, other):
-        """compare with >="""
+        """Compare with >="""
         return (self.area() >= other.area())
 
     def __eq__(self, other):
-        """compare with =="""
+        """Compare with =="""
         return (self.area() == other.area())
 
     def __ne__(self, other):
-        """compare with !="""
+        """Compare with !="""
         return (self.area() != other.area())
 
     def area(self):
-        """return area"""
+        """Return area"""
         return (self.__size ** 2)
 
     @property
     def size(self):
-        """getter for size"""
+        """Getter for size"""
         return (self.__size ** 2)
 
     @size.setter
     def size(self, value):
-        """setter for size"""
+        """Setter for size"""
         if (type(value) is not int) or (type(value) is not float):
             if value < 0:
                 raise ValueError("size must be >= 0")
