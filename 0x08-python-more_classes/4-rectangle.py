4296baa943206cda28007587de615d65e8f51580
Makbelhailu <mcwelson92@gmail.com>
2022-09-19 22:00:06 +0300

    python is cool

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -27,7 +27,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -48,9 +48,8 @@ class Rectangle:
         """primt area"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        else:
-            return (("#" * self.__width + "\n") * self.__height)[:-1]
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
 
     def __repr__(self):
         '''Returns formal string representation...'''
-        return "Rectangle(" + str(self.__width) "," + str(self.__height) + ")"
+        return "Rectangle(" + str(self.width) "," + str(self.height) + ")"
