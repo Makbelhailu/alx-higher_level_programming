45d76f18c2a96bbaea575dd37f28d386dcfc6f9b
Makbelhailu <mcwelson92@gmail.com>
2022-09-19 21:45:07 +0300

    python is cool

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""module of class Rectangle"""
+
+
+class Rectangle:
+    """Class of Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return (("#" * self.__width + "\n") * self.__height)[:-1]
+
+    def __repr__(self):
+        return ("Rectangle(" + str(self.width) "," + str(self.height) + ")")
