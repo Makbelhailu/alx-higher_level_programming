d6a5074544f49c7eadab7c953b57c70d61464e0b
Makbelhailu <mcwelson92@gmail.com>
2022-09-22 13:52:24 +0300

    python is cool

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A function that print z name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """print the full name
+    Args:
+        first_name
+        last_name
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
