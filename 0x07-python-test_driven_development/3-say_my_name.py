81e8b951df7c3bb54c03de4dc993147f1cadd8da
Makbelhailu <mcwelson92@gmail.com>
2022-09-22 13:55:14 +0300

    python is cool

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,7 +15,6 @@ def say_my_name(first_name, last_name=""):
     print(f"My name is {first_name} {last_name}")
 
 
-
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/3-say_my_name.txt")
