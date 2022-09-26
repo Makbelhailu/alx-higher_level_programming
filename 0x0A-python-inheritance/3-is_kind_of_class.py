4d0eb5c58d2764206489b7a9a57af5dca54d2a87
Makbelhailu <mcwelson92@gmail.com>
2022-09-26 19:58:47 +0300

    python is cool

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""A functin that tests isinstance"""
 
 
 def is_kind_of_class(obj, a_class):
+    """return true when the isinstance is true"""
     return (isinstance(obj, a_class))
