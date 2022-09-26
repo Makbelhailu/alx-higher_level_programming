2dc99bbd0ffd043a855bc0d8cb54b50d33faf2db
Makbelhailu <mcwelson92@gmail.com>
2022-09-26 19:51:27 +0300

    python is cool

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function returns True if isinstance is true"""
+
+
+def is_same_class(obj, a_class):
+    """return True if it is instance"""
+    return (isinstance(obj, a_class))
