81f59dfa29f619a4f13b2b61ed78a2ac63b50de2
Makbelhailu <mcwelson92@gmail.com>
2022-09-26 21:20:27 +0300

    python is cool

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""A class with method area"""
+"""
+A class with method area
+"""
 
 
 class BaseGeometry:
     """raise an exception"""
-
     def area(self):
-        """exception raiser"""
+        """raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validet the int"""
+        """Validates value to be an int grater than 0.
+        Args:
+            name (str): name of the value
+            value (unknown): to be validated.
+        Raises:
+            TypeError: if value is not an int.
+            ValueError: if value is <= 0.
+        """
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
