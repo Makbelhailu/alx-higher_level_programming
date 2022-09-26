8d332ffeda05558b67f8eecf17c1be424e7c3e46
Makbelhailu <mcwelson92@gmail.com>
2022-09-26 20:14:19 +0300

    python is cool

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,6 @@
 def inherits_from(obj, a_class):
     """Return True when issubclass is true"""
 
-    return (issubclass(type(obj), a_class))
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
