1a03ee0dd12edc678d19410b73632b55e78eacd7
Makbelhailu <mcwelson92@gmail.com>
2022-09-26 23:17:32 +0300

    python is cool

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@ def add_attribute(obj, name, value):
     """methods to add arttributes"""
 
     if hasattr(obj, "__dict__") or \
-       (hasattr(obj, "__slots__") and name in obj.__slots__):
-           setattr(obj, name, value)
+            (hasattr(obj, "__slots__") and name in obj.__slots__):
+        setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
