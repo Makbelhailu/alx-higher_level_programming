c30bea9695d1811614236b8c7ceff30e3b0db4d1
Makbelhailu <mcwelson92@gmail.com>
2022-09-26 21:57:13 +0300

    python is cool

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A class with method area
+"""
+
+
+class BaseGeometry:
+    """raise an exception"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value to be an int grater than 0.
+        Args:
+            name (str): name of the value
+            value (unknown): to be validated.
+        Raises:
+            TypeError: if value is not an int.
+            ValueError: if value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        super().integer_validator("height", self.__height)
+        super().integer_validator("width", self.__width)
