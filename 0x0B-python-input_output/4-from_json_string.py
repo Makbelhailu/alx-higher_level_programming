f51e114315e772cccb9e80e4e831ba176222bc55
Makbelhailu <mcwelson92@gmail.com>
2022-09-28 19:08:11 +0300

    python is cool

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """convert to jason to str"""
-
 import json
 
 
- def from_json_string(my_str):
+def from_json_string(my_str):
     """fonction to convert"""
     return json.loads(my_str)
