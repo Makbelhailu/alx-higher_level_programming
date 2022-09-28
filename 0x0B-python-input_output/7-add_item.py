cae544eb1bf6905f5e24925c8ebd7c41aa064b1e
Makbelhailu <mcwelson92@gmail.com>
2022-09-28 19:27:02 +0300

    python is cool

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,12 @@ from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-filenane =  "add_item.json"
+filenane = "add_item.json"
 
 
 try:
     json_list = load_from_json_file(file_name)
-except:
+except Exception:
     json_list = []
     for i in argv[1:]:
         json_list.append(i)
