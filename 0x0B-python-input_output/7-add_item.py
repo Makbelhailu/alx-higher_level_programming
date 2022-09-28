4523ce5037b312abf701dbdf8d88381a388400aa
Makbelhailu <mcwelson92@gmail.com>
2022-09-28 19:33:53 +0300

    python is cool

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""mix all tasks"""
+"""Module for a script that adds all arguments to a Python list"""
 import json
 from sys import argv
-
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 
 filename = "add_item.json"
 
@@ -13,7 +13,7 @@ try:
     json_list = load_from_json_file(file_name)
 except Exception:
     json_list = []
-    for i in argv[1:]:
-        json_list.append(i)
+for i in argv[1:]:
+    json_list.append(i)
 
-    save_to_json_file(json_list, filename)
+save_to_json_file(json_list, filename)
