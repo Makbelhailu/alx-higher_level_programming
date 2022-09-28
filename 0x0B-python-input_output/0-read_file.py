883b2a0db81dad86451d04e4e35387c14af06595
Makbelhailu <mcwelson92@gmail.com>
2022-09-28 18:38:53 +0300

    python is cool

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""function prints the file content"""
+
+
 def read_file(filename=""):
+    """printer func"""
     with open(filename, 'r', encoding="utf-8") as f:
         file = f.read()
-        f.close()
-        print(file)
+        print(file, end="")
