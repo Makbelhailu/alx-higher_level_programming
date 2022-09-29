cd25e9f5ddc69e20db9c1964ac589ee1481410f8
Makbelhailu <mcwelson92@gmail.com>
2022-09-29 05:30:39 +0300

    python is cool

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""class with personal info"""
+"""Student class Module"""
 
 
-class Student:
-    """manage studentes info"""
-
+class Student():
+    """Student class"""
     def __init__(self, first_name, last_name, age):
-        """run first when class called"""
+        """Initializes Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """return the dictionary"""
+        """Gets dict with filter"""
         if type(attrs) is list and all([type(attr) == str for attr in attrs]):
             return {key: val for key, val in self.__dict__.items() if key in
                     attrs}
         else:
-            return self.__dict__
+            return self.__dict__.copy()
 
     def reload_from_json(self, json):
-        """reload json from given argument"""
-        for key, val in json.items():
-            self.__dict__[key] = val
+        """Method for loading attributes from json"""
+        for k, val in json.items():
+            self.__dict__[k] = val
