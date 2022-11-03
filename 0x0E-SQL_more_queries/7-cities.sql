e8cdf9b0ac944512a0c308f4624920770037a984
Makbelhailu <mcwelson92@gmail.com>
2022-11-03 03:55:10 +0300

    python is cool

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- create table with forign key
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+	state_id INT NOT NULL FOREIGN KEY (id) REFERENCES states(id),
+	name VARCHAR(256)) NOT NULL;
