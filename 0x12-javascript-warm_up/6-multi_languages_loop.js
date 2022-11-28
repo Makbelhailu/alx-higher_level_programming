c0d0fdb5d82b9ca2a972c2a7482636ed033f01f2
Makbelhailu <mcwelson92@gmail.com>
2022-11-28 17:35:37 +0300

    python is cool

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lists = ['C is fun', 'Python is cool', 'JavaScript is amazing' ];
-for(var x = 0; x < lists.length; x++) {
-	console.log(lists[x]);
+const lists = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let x = 0; x < lists.length; x++) {
+  console.log(lists[x]);
 }
