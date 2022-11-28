0c855aad5a3267aea00a4aebbb7e7367e172ebb9
Makbelhailu <mcwelson92@gmail.com>
2022-11-28 19:10:33 +0300

    python is cool

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+if (process.argv.length === 3) {
   console.log(1);
+} else if (process.argv.length < 3) {
+  console.log(0);
 } else {
   const arg = process.argv.map(Number)
     .slice(2, process.argv.length)
