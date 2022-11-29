0e567609d9ce7ff081c6dd6907e2b63d50fce8da
Makbelhailu <mcwelson92@gmail.com>
2022-11-29 19:45:55 +0300

    python is cool

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+	    this.width = w;
+	    this.height = h;
+    }
+  }
+
+  print () {
+    const X = 'X';
+    for (let x = 0; x < this.height; x++) { console.log(X.repeat(this.width)); }
+  }
+};
