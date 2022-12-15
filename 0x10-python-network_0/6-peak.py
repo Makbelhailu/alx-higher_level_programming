2f4bf77ce889ae205153f4fa5b0f4f3c21bac117
Makbelhailu <mcwelson92@gmail.com>
2022-12-15 23:30:51 +0300

    python is cool

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Finds peak of list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    lo = 0
+    h = len(list_of_integers)
+    mid = ((h - lo) // 2) + lo
+    mid = int(mid)
+    if h == 1:
+        return list_of_integers[0]
+    if h == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
